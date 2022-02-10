0556b80460c6d2a29930bc9c227eaf1d8b80b7b3
burakpolater <bpolater@gmail.com>
2022-02-10 23:18:19 +0300

    helpers dosyası oluşturuldu

--- utils/helpers.py
+++ utils/helpers.py
@@ -6,59 +6,15 @@ class MyHelpers:
 
     def get_spark_session(self, session_params: dict) -> SparkSession:
         ## Put your code here.
-        spark = (
-            SparkSession.builder
-                .appName(session_params["appName"])
-                .enableHiveSupport()
-                .master(session_params["master"])
-                .config("spark.executor.memory", session_params["executor_memory"])
-                .config("spark.executor.instances", session_params["executor_instances"])
-                .config("spark.executor.cores", session_params["executor_cores"])
-                .getOrCreate()
-        )
+        spark = (SparkSession.builder
+                 .appName("Read From Kafka")
+                 .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1")
+                 .getOrCreate())
+                #TODO: ELASTIC CONFIG EKLENECEK
+        spark.sparkContext.setLogLevel('ERROR')
         return spark
 
-    def get_data(self, spark_session: SparkSession, read_params: dict) -> DataFrame:
-        ## Put your code here
-        df = (spark_session.read.format(read_params["format"])
-              .option("header", read_params["header"])
-              .option("sep", read_params["sep"])
-              .option("inferSchema", read_params["inferSchema"])
-              .load(read_params["path"])
-              )
-              
-        return df
-
-    def format_dates(self, date_cols: list, input_df: DataFrame) -> DataFrame:
-        ## Make timestamp of string dates/ts
-        df = input_df.withColumn(date_cols[0] + "_new", 
-                        F.to_timestamp(F.col(date_cols[0]), 'yyyy-MM-dd HH:mm:ss.SSSSSSx')) \
-                    .withColumn(date_cols[1] + "_new", 
-                        F.to_timestamp(F.col(date_cols[1]), 'yyyy-MM-dd HH:mm:ss.SSSSSx'))
-        return df
-
-    def make_nulls_to_unknown(self, input_df: DataFrame) -> DataFrame:
-        ## Replace Null/None/Empty values with Unknown in string columns
-        str_cols = [col[0] for col in input_df.dtypes if col[1] == "string"]
-        df = input_df
-        for my_col in str_cols:
-            df = df.withColumn(my_col, F.when(
-                (F.col(my_col).isNull() | (F.col(my_col) == "None")),
-                "Unknown").otherwise(F.col(my_col)))
-
-        return df
-
-    def trim_str_cols(self, input_df) -> DataFrame:
-        ## trim all string cols
-        df = input_df
-        str_col = [col for col in df.columns if col.startswith('string')]
-
-        for i in range(len(str_col)):
-            df.withColumn(str_col[i],F.trim(F.col(str_col[i])))
-        
-        return df
-
-    def write_to_hive(self, input_df: DataFrame):
+    def write_to_elastic(self, input_df: DataFrame): #TODO: GÜNCELLENCEK
         # write to hive test1 database cars_cleaned table in avro format
         input_df.write \
             .format("avro") \
@@ -67,3 +23,99 @@ class MyHelpers:
             .saveAsTable("default.cars_cleaned")
 
         return print("Successfully uploaded to Hive")
+
+    def switch_tr_day(day_index):
+        my_dict = {
+            1: 'Pazar',
+            2: 'Pazartesi',
+            3: 'Salı',
+            4: 'Çarşamba',
+            5: 'Perşembe',
+            6: 'Cuma',
+            7: 'Cumartesi'
+        }
+
+        return my_dict.get(day_index)
+
+    def switch_month_day(month_index):
+        my_dict = {
+            1: 'Ocak',
+            2: 'Subat',
+            3: 'Mart',
+            4: 'Nisan',
+            5: 'Mayis',
+            6: 'Haziran',
+            7: 'Temmuz',
+            8: 'Agustos',
+            9: 'Eylul',
+            10: 'Ekim',
+            11: 'Kasim',
+            12: 'Aralik'
+        }
+
+        return my_dict.get(month_index)
+
+    def haversine(lon1, lat1, lon2, lat2):
+        """
+        Calculate the great circle distance in kilometers between two points
+        on the earth (specified in decimal degrees)
+        """
+        # convert decimal degrees to radians
+        lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+        # haversine formula
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+        c = 2 * asin(sqrt(a))
+        r = 6371  # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+        return c * r
+
+    def format_dates_ML(self, input_df: DataFrame) -> DataFrame:
+        df = input_df.withColumn("pickup_datetime",
+                                   F.to_timestamp(F.col("pickup_datetime"), "yyyy-MM-dd HH🇲🇲ss")) \
+            .withColumn("dropoff_datetime",
+                        F.to_timestamp(F.col("dropoff_datetime"), "yyyy-MM-dd HH🇲🇲ss")) \
+            .withColumn("pickup_year",
+                        F.year(F.to_date(F.col("pickup_datetime")))) \
+            .withColumn("pickup_month",
+                        F.month(F.to_date(F.col("pickup_datetime")))) \
+            .withColumn("pickup_dayofweek",
+                        F.dayofweek(F.to_date(F.col("pickup_datetime")))) \
+            .withColumn("pickup_hour",
+                        F.hour(F.col("pickup_datetime"))) \
+            .withColumn("pickupDayofWeek_TR",
+                        MyHelpers.switch_tr_day(F.col("pickup_dayofweek"))) \
+            .withColumn("pickupMonth_TR",
+                        MyHelpers.switch_month_day(F.col("pickup_month"))) \
+            .withColumn("haversine_distance(km)",
+                        MyHelpers.haversine(F.col("pickup_longitude"), F.col("pickup_latitude"),
+                                           F.col("dropoff_longitude"),
+                                           F.col("dropoff_latitude")))
+        return df
+
+    def format_dates_DE(self, input_df: DataFrame) -> DataFrame:
+        df = input_df.withColumn("pickup_datetime",
+                                   F.to_timestamp(F.col("pickup_datetime"), "yyyy-MM-dd HH🇲🇲ss")) \
+            .withColumn("dropoff_datetime",
+                        F.to_timestamp(F.col("dropoff_datetime"), "yyyy-MM-dd HH🇲🇲ss")) \
+            .withColumn("pickup_year",
+                        F.year(F.to_date(F.col("pickup_datetime")))) \
+            .withColumn("pickup_month",
+                        F.month(F.to_date(F.col("pickup_datetime")))) \
+            .withColumn("pickup_dayofweek",
+                        F.dayofweek(F.to_date(F.col("pickup_datetime")))) \
+            .withColumn("pickup_hour",
+                        F.hour(F.col("pickup_datetime"))) \
+            .withColumn("pickupDayofWeek_TR",
+                        MyHelpers.switch_tr_day(F.col("pickup_dayofweek"))) \
+            .withColumn("pickupMonth_TR",
+                        MyHelpers.switch_month_day(F.col("pickup_month"))) \
+            .withColumn("haversine_distance(km)",
+                        MyHelpers.haversine(F.col("pickup_longitude"), F.col("pickup_latitude"),
+                                           F.col("dropoff_longitude"),
+                                           F.col("dropoff_latitude")))
+
+            #TODO: ELASTİC İÇİN FEATURELAR EKLE
+
+        return df
